b0bb4961180b02a54c2f1b85f7e1a04b34892fb6
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-08-06 09:06:46 -0500

    added normalize and layout code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ require './config/environment.rb'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
   get '/' do
     erb :index
   end
